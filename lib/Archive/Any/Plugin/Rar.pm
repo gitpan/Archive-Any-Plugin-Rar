cacd1b32a0871337b6491bcaf93fc1aa0e70253a
Dmitriy V. Simonov <dsimonov@gmail.com>
2013-02-18 01:39:24 +0000

    Import of MRGRIPH/Archive-Any-Plugin-Rar-0.02 from CPAN.
    gitpan-cpan-distribution: Archive-Any-Plugin-Rar
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MRGRIPH/Archive-Any-Plugin-Rar-0.02.tar.gz
    gitpan-cpan-author:       MRGRIPH
    gitpan-cpan-maturity:     released

--- lib/Archive/Any/Plugin/Rar.pm
+++ lib/Archive/Any/Plugin/Rar.pm
@@ -13,7 +13,7 @@ Archive::Any::Plugin::Rar - Archive::Any wrapper around Archive::Rar
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
@@ -25,6 +25,7 @@ Do not use this module directly.  Instead, use Archive::Any.
 sub can_handle {
     return(
            'application/x-rar',
+           'application/x-rar-compressed',
           );
 }
 
@@ -38,7 +39,7 @@ sub files {
 sub extract {
     my ( $self, $file ) = @_;
 
-    my $t = Archive::Rar->new( -archive => $file );
+    my $t = Archive::Rar->new( -archive => $file, -quiet   => 'True' );
     return $t->Extract;
 }
 
@@ -58,6 +59,10 @@ Dmitriy V. Simonov, C<< <dsimonov at gmail.com> >>
 =head1 ACKNOWLEDGEMENTS
 
 Thanks to Clint Moore for L<Archive::Any> 
+
+Thanks to C<< <d_ion at mail.ru> >> for minor fix
+
+Thanks to C<< <ksuri at cpan.org> >> for major fix
 
 =head1 COPYRIGHT & LICENSE
 
